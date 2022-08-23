6895018163496aeca51a1a2a62c6d40b4e939609
Steven Worley <sj.worley88@gmail.com>
2022-08-24 08:29:35 +1000

    Add dockerfile base image scan.
    Add dockerfile base image scan.
    
    Convert to util.
    
    Update checks and test coverage.
    
    Update sorting for tests.

--- pkg/docker/base_image_test.go
+++ pkg/docker/base_image_test.go
@@ -1,35 +1,44 @@
 package docker_test
 
 import (
+	"sort"
 	"testing"
 
-	"github.com/salsadigitalauorg/shipshape/internal"
 	"github.com/salsadigitalauorg/shipshape/pkg/docker"
+	"github.com/salsadigitalauorg/shipshape/pkg/shipshape"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestDockerfileCheck(t *testing.T) {
+	assert := assert.New(t)
 	c := docker.BaseImageCheck{
 		Allowed: []string{"bitnami/kubectl"},
 		Paths:   []string{"./fixtures/compose-dockerfile"},
 	}
 	c.RunCheck()
-	if msg, ok := internal.EnsurePasses(t, &c.CheckBase, []string{"Dockerfiles adhere to the policy"}); !ok {
-		t.Error(msg)
-	}
+	assert.Equal(shipshape.Pass, c.Result.Status)
+	assert.EqualValues(
+		[]string{"service1 is using valid base images"},
+		c.Result.Passes,
+	)
 }
 
 func TestInvalidDockerfileCheck(t *testing.T) {
+	assert := assert.New(t)
 	c := docker.BaseImageCheck{
 		Allowed: []string{"bitnami/redis"},
 		Paths:   []string{"./fixtures/compose-dockerfile"},
 	}
 	c.RunCheck()
-	if msg, ok := internal.EnsureFailures(t, &c.CheckBase, []string{"service1 is using invalid base image FROM bitnami/kubectl"}); !ok {
-		t.Error(msg)
-	}
+	assert.Equal(shipshape.Fail, c.Result.Status)
+	assert.EqualValues(
+		[]string{"service1 is using invalid base image bitnami/kubectl"},
+		c.Result.Failures,
+	)
 }
 
 func TestValidImage(t *testing.T) {
+	assert := assert.New(t)
 	c := docker.BaseImageCheck{
 		Allowed: []string{
 			"bitnami/kubectl",
@@ -40,12 +49,21 @@ func TestValidImage(t *testing.T) {
 		Paths: []string{"./fixtures/compose-image"},
 	}
 	c.RunCheck()
-	if msg, ok := internal.EnsurePasses(t, &c.CheckBase, []string{"Dockerfiles adhere to the policy"}); !ok {
-		t.Error(msg)
-	}
+	assert.Equal(shipshape.Pass, c.Result.Status)
+	sort.Strings(c.Result.Passes)
+	assert.EqualValues(
+		[]string{
+			"service1 is using valid base images",
+			"service2 is using valid base images",
+			"service3 is using valid base images",
+			"service4 is using valid base images",
+		},
+		c.Result.Passes,
+	)
 }
 
 func TestInvalidImageCheck(t *testing.T) {
+	assert := assert.New(t)
 	c := docker.BaseImageCheck{
 		Allowed: []string{
 			"bitnami/kubectl",
@@ -55,36 +73,44 @@ func TestInvalidImageCheck(t *testing.T) {
 		Paths: []string{"./fixtures/compose-image"},
 	}
 	c.RunCheck()
-
-	if msg, ok := internal.EnsureFailures(t, &c.CheckBase, []string{"service4 is using an invalid base image bitnami/mongodb"}); !ok {
-		t.Error(msg)
-	}
+	assert.Equal(shipshape.Fail, c.Result.Status)
+	assert.EqualValues(
+		[]string{"service4 is using invalid base image bitnami/mongodb"},
+		c.Result.Failures,
+	)
 }
 
 func TestDockerfileWarning(t *testing.T) {
+	assert := assert.New(t)
 	c := docker.BaseImageCheck{
 		Allowed: []string{"bitnami/kubectl"},
 		Paths:   []string{"./fixtures/compose-dockerfile-missing"},
 	}
 	c.RunCheck()
-	if msg, ok := internal.EnsureWarnings(t, &c.CheckBase, []string{"Unable to find Dockerfile"}); !ok {
-		t.Error(msg)
-	}
+	assert.Equal(shipshape.Pass, c.Result.Status)
+	assert.EqualValues(
+		[]string{"Unable to find Dockerfile"},
+		c.Result.Warnings,
+	)
 }
 
 func TestExcludeServiceSingle(t *testing.T) {
+	assert := assert.New(t)
 	c := docker.BaseImageCheck{
 		Allowed: []string{"bitnami/redis"},
 		Exclude: []string{"service1"},
 		Paths:   []string{"./fixtures/compose-dockerfile"},
 	}
 	c.RunCheck()
-	if msg, ok := internal.EnsurePasses(t, &c.CheckBase, []string{"Dockerfiles adhere to the policy"}); !ok {
-		t.Error(msg)
-	}
+	assert.Equal(shipshape.Pass, c.Result.Status)
+	assert.EqualValues(
+		[]string(nil),
+		c.Result.Passes,
+	)
 }
 
 func TestExcludeServiceMany(t *testing.T) {
+	assert := assert.New(t)
 	c := docker.BaseImageCheck{
 		Allowed: []string{
 			"bitnami/kubectl",
@@ -95,7 +121,28 @@ func TestExcludeServiceMany(t *testing.T) {
 		Paths:   []string{"./fixtures/compose-image"},
 	}
 	c.RunCheck()
-	if msg, ok := internal.EnsurePasses(t, &c.CheckBase, []string{"Dockerfiles adhere to the policy"}); !ok {
-		t.Error(msg)
+	assert.Equal(shipshape.Pass, c.Result.Status)
+	sort.Strings(c.Result.Passes)
+	assert.EqualValues(
+		[]string{
+			"service1 is using valid base images",
+			"service2 is using valid base images",
+			"service4 is using valid base images",
+		},
+		c.Result.Passes,
+	)
+}
+
+func TestDeprecatedImage(t *testing.T) {
+	assert := assert.New(t)
+	c := docker.BaseImageCheck{
+		Deprecated: []string{"bitnami/kubectl"},
+		Paths:      []string{"./fixtures/compose-dockerfile"},
 	}
+	c.RunCheck()
+	assert.Equal(shipshape.Pass, c.Result.Status)
+	assert.EqualValues(
+		[]string{"service1 is using deprecated image bitnami/kubectl"},
+		c.Result.Warnings,
+	)
 }
