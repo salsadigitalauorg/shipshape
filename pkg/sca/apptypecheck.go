519f87feddb89832e27fdb064682c8290b2206da
Steve Worley <sj.worley88@gmail.com>
2023-01-05 12:24:07 +1000

    Update the apptype check.

--- pkg/sca/apptypecheck.go
+++ pkg/sca/apptypecheck.go
@@ -1,17 +1,45 @@
 package sca
 
 import (
+	"os"
+	"path/filepath"
+
 	"github.com/salsadigitalauorg/shipshape/pkg/shipshape"
 	"github.com/salsadigitalauorg/shipshape/pkg/utils"
 )
 
 const AppType shipshape.CheckType = "sca:application_type"
 
+/*
+Example:
+	threshold: 20
+	disallowed:
+		- symfyony
+	markers:
+		symfony:
+			-     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+	dirs:
+		symfony:
+			- bin
+			- config
+			- public
+	dependencies:
+		symfony:
+			- symfony/runtime
+			- symfony/symfony
+			- symfony/framework
+
+@TODO: currently only supports composer for dependency lookups.
+*/
 type AppTypeCheck struct {
 	shipshape.CheckBase `yaml:",inline"`
-	Disallowed          []string `yaml:"disallowed"`
-	Paths               []string `yaml:"paths"`
-	Threshold           int      `yaml:"threshold"`
+	Threshold           int                 `yaml:"threshold"`
+	Disallowed          []string            `yaml:"disallowed"`
+	Entrypoint          string              `yaml:"entrypoint"`
+	Paths               []string            `yaml:"paths"`
+	Markers             map[string][]string `yaml:"markers"`
+	Dirs                map[string][]string `yaml:"dirs"`
+	Dependencies        map[string][]string `yaml:"dependencies"`
 }
 
 func (c *AppTypeCheck) Merge(mergeCheck shipshape.Check) error {
@@ -28,121 +56,52 @@ func (c *AppTypeCheck) Merge(mergeCheck shipshape.Check) error {
 }
 
 func (c *AppTypeCheck) RunCheck() {
+
+	if c.Threshold == 0 {
+		c.Threshold = 30
+	}
+
+	if c.Entrypoint == "" {
+		c.Entrypoint = "index.php"
+	}
+
 	for _, path := range c.Paths {
-		if utils.StringSliceContains(c.Disallowed, "wordpress") && isWordpress(path) > c.Threshold {
-			c.AddFail("Wordpress detected at " + path)
-		}
-		if utils.StringSliceContains(c.Disallowed, "symfony") && isSymfony(path) > c.Threshold {
-			c.AddFail("Symfony detected at " + path)
-		}
-		if utils.StringSliceContains(c.Disallowed, "laravel") && isLaravel(path) > c.Threshold {
-			c.AddFail("Laravel detected at " + path)
-		}
-		if utils.StringSliceContains(c.Disallowed, "drupal") && isDrupal(path) > c.Threshold {
-			c.AddFail("Drupal detected at " + path)
-		}
-	}
-	if len(c.Result.Failures) == 0 {
-		c.AddPass("No invalid application types detected")
-		c.Result.Status = shipshape.Pass
-	}
-}
+		entrypoints, _ := utils.Glob(path, c.Entrypoint)
 
-func isWordpress(path string) int {
-	likelihood := 0
+		for _, framework := range c.Disallowed {
+			likelihood := 0
+			if markers, ok := c.Markers[framework]; ok {
+				for _, marker := range markers {
+					for _, e := range entrypoints {
+						if f, _ := utils.FileContains(e, marker); f {
+							likelihood += 5
+						}
+					}
+				}
+			}
 
-	marker := " * @package WordPress"
-	paths, _ := utils.Glob(path, "index.php")
-	for _, index := range paths {
-		if f, _ := utils.FileContains(index, marker); f {
-			likelihood += 10
+			if dirs, ok := c.Dirs[framework]; ok {
+				filepath.Walk(path, func(path string, f os.FileInfo, err error) error {
+					if f.IsDir() && utils.StringSliceContains(dirs, f.Name()) {
+						likelihood += 5
+					}
+					return nil
+				})
+			}
+
+			if deps, ok := c.Dependencies[framework]; ok {
+				if hasDep, _ := utils.HasComposerDependency(path, deps); hasDep {
+					likelihood += 10
+				}
+			}
+
+			if likelihood > c.Threshold {
+				c.AddFail(framework + " detected at " + path)
+			}
 		}
-	}
-
-	dirs := []string{"wp-admin", "wp-content", "wp-includes"}
-	for _, dir := range dirs {
-		if d, _ := utils.IsDirectory(dir); d {
-			likelihood += 10
-		}
-	}
-
-	// Wordpress is not detected.
-	return likelihood
-}
-
-func isDrupal(path string) int {
-	likelihood := 0
-
-	paths, _ := utils.Glob(path, "index.php")
-	marker := "use Drupal\\Core\\DrupalKernel;"
-	for _, index := range paths {
-		if f, _ := utils.FileContains(index, marker); f {
-			likelihood += 10
+		if len(c.Result.Failures) == 0 {
+			c.AddPass("No invalid application types detected")
+			c.Result.Status = shipshape.Pass
 		}
 	}
-
-	deps := []string{"drupal/core-recommended"}
-	if hasDep, _ := utils.HasComposerDependency(path, deps); hasDep {
-		likelihood += 10
-	}
-
-	dirs := []string{"web", "docroot"}
-	for _, dir := range dirs {
-		if d, _ := utils.IsDirectory(dir); d {
-			likelihood += 5
-		}
-	}
-
-	return likelihood
-}
-
-func isSymfony(path string) int {
-	likelihood := 0
-	marker := "    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);"
-	paths, _ := utils.Glob(path, "index.php")
-	for _, index := range paths {
-		if f, _ := utils.FileContains(index, marker); f {
-			likelihood += 10
-		}
-	}
-
-	// Check composer.json
-	deps := []string{"symfony/runtime", "symfony/symfony", "symfony/framework"}
-	if hasDep, _ := utils.HasComposerDependency(path, deps); hasDep {
-		likelihood += 10
-	}
-
-	dirs := []string{"bin", "config", "public"}
-	for _, dir := range dirs {
-		if d, _ := utils.IsDirectory(dir); d {
-			likelihood += 5
-		}
-	}
-
-	return likelihood
-}
-
-func isLaravel(path string) int {
-	likelihood := 0
-	marker := "use Illuminate\\Contracts\\Http\\Kernel;"
-	paths, _ := utils.Glob(path, "index.php")
-	for _, index := range paths {
-		if f, _ := utils.FileContains(index, marker); f {
-			likelihood += 10
-		}
-	}
-
-	deps := []string{"laravel/framework", "laravel/tinker"}
-	if hasDep, _ := utils.HasComposerDependency(path, deps); hasDep {
-		likelihood += 10
-	}
-
-	dirs := []string{"bin", "config", "public"}
-	for _, dir := range dirs {
-		if d, _ := utils.IsDirectory(dir); d {
-			likelihood += 5
-		}
-	}
-
-	return likelihood
 }
--- pkg/sca/apptypecheck_test.go
+++ pkg/sca/apptypecheck_test.go
@@ -11,14 +11,22 @@ import (
 func TestIsDrupalCheck(t *testing.T) {
 	assert := assert.New(t)
 	c := sca.AppTypeCheck{
-		Disallowed: []string{"drupal"},
-		Threshold:  1,
-		Paths:      []string{"./fixtures/drupal"},
+		Disallowed:   []string{"drupal"},
+		Threshold:    1,
+		Markers:      make(map[string][]string),
+		Dirs:         make(map[string][]string),
+		Dependencies: make(map[string][]string),
+		Paths:        []string{"./fixtures/drupal"},
 	}
+
+	c.Markers["drupal"] = []string{"use Drupal\\Core\\DrupalKernel;"}
+	c.Dirs["drupal"] = []string{"web", "docroot"}
+	c.Dependencies["drupal"] = []string{"drupal/core-recommended"}
+
 	c.RunCheck()
 	assert.Equal(shipshape.Fail, c.Result.Status)
 	assert.EqualValues(
-		[]string{"Drupal detected at ./fixtures/drupal"},
+		[]string{"drupal detected at ./fixtures/drupal"},
 		c.Result.Failures,
 	)
 }
@@ -26,9 +34,12 @@ func TestIsDrupalCheck(t *testing.T) {
 func TestThreshold(t *testing.T) {
 	assert := assert.New(t)
 	c := sca.AppTypeCheck{
-		Disallowed: []string{"drupal"},
-		Threshold:  100,
-		Paths:      []string{"./fixtures/drupal"},
+		Disallowed:   []string{"drupal"},
+		Threshold:    100,
+		Markers:      make(map[string][]string),
+		Dirs:         make(map[string][]string),
+		Dependencies: make(map[string][]string),
+		Paths:        []string{"./fixtures/drupal"},
 	}
 	c.RunCheck()
 	assert.Equal(shipshape.Pass, c.Result.Status)
@@ -37,14 +48,21 @@ func TestThreshold(t *testing.T) {
 func TestIsWordpressCheck(t *testing.T) {
 	assert := assert.New(t)
 	c := sca.AppTypeCheck{
-		Disallowed: []string{"wordpress"},
-		Threshold:  1,
-		Paths:      []string{"./fixtures/wordpress"},
+		Disallowed:   []string{"wordpress"},
+		Threshold:    1,
+		Markers:      make(map[string][]string),
+		Dirs:         make(map[string][]string),
+		Dependencies: make(map[string][]string),
+		Paths:        []string{"./fixtures/wordpress"},
 	}
+
+	c.Markers["wordpress"] = []string{" * @package WordPress"}
+	c.Dirs["wordpress"] = []string{"wp-admin", "wp-content", "wp-includes"}
+
 	c.RunCheck()
 	assert.Equal(shipshape.Fail, c.Result.Status)
 	assert.EqualValues(
-		[]string{"Wordpress detected at ./fixtures/wordpress"},
+		[]string{"wordpress detected at ./fixtures/wordpress"},
 		c.Result.Failures,
 	)
 }
@@ -52,10 +70,17 @@ func TestIsWordpressCheck(t *testing.T) {
 func TestIsSymfonyCheck(t *testing.T) {
 	assert := assert.New(t)
 	c := sca.AppTypeCheck{
-		Disallowed: []string{"symfony"},
-		Threshold:  1,
-		Paths:      []string{"./fixtures/symfony"},
+		Disallowed:   []string{"symfony"},
+		Threshold:    1,
+		Markers:      make(map[string][]string),
+		Dirs:         make(map[string][]string),
+		Dependencies: make(map[string][]string),
+		Paths:        []string{"./fixtures/symfony"},
 	}
+	c.Markers["symfony"] = []string{"    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);"}
+	c.Dirs["symfony"] = []string{"bin", "config", "public"}
+	c.Dependencies["symfony"] = []string{"symfony/runtime", "symfony/symfony", "symfony/framework"}
+
 	c.RunCheck()
 	assert.Equal(shipshape.Fail, c.Result.Status)
 	assert.EqualValues(
@@ -67,24 +92,56 @@ func TestIsSymfonyCheck(t *testing.T) {
 func TestIsLaravelCheck(t *testing.T) {
 	assert := assert.New(t)
 	c := sca.AppTypeCheck{
-		Disallowed: []string{"laravel"},
-		Threshold:  1,
-		Paths:      []string{"./fixtures/laravel"},
+		Disallowed:   []string{"laravel"},
+		Threshold:    1,
+		Markers:      make(map[string][]string),
+		Dirs:         make(map[string][]string),
+		Dependencies: make(map[string][]string),
+		Paths:        []string{"./fixtures/laravel"},
 	}
+	c.Markers["laravel"] = []string{"use Illuminate\\Contracts\\Http\\Kernel;"}
+	c.Dirs["laravel"] = []string{"config", "public"}
+	c.Dependencies["laravel"] = []string{"laravel/framework", "laravel/tinker"}
+
 	c.RunCheck()
 	assert.Equal(shipshape.Fail, c.Result.Status)
 	assert.EqualValues(
-		[]string{"Laravel detected at ./fixtures/laravel"},
+		[]string{"laravel detected at ./fixtures/laravel"},
 		c.Result.Failures,
+	)
+}
+
+func TestMultipleTypes(t *testing.T) {
+	assert := assert.New(t)
+	c := sca.AppTypeCheck{
+		Disallowed:   []string{"laravel", "symfony", "wordpress"},
+		Threshold:    1,
+		Markers:      make(map[string][]string),
+		Dirs:         make(map[string][]string),
+		Dependencies: make(map[string][]string),
+		Paths:        []string{"./fixtures/drupal"},
+	}
+	c.Dirs["laravel"] = []string{"config", "public"}
+	c.Dirs["symfony"] = []string{"config", "public"}
+	c.Dirs["wordpress"] = []string{"wp-admin"}
+
+	c.RunCheck()
+	assert.Equal(shipshape.Pass, c.Result.Status)
+	assert.EqualValues(
+		[]string{"No invalid application types detected"},
+		c.Result.Passes,
 	)
 }
 
 func TestOK(t *testing.T) {
 	assert := assert.New(t)
 	c := sca.AppTypeCheck{
-		Disallowed: []string{"laravel", "wordpress", "symfony"},
-		Threshold:  1,
-		Paths:      []string{"./fixtures/drupal"},
+		Disallowed:   []string{"laravel", "wordpress", "symfony"},
+		Threshold:    1,
+		Markers:      make(map[string][]string),
+		Dirs:         make(map[string][]string),
+		Dependencies: make(map[string][]string),
+		Paths:        []string{"./fixtures/drupal"},
 	}
 	c.RunCheck()
 	assert.Equal(shipshape.Pass, c.Result.Status)
