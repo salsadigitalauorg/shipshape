4a913ab42da3fb07fe2e9469ac61cf3c5a05f674
Yusuf A. Hasan Miyan <yusufh@protonmail.com>
2024-02-29 13:10:52 +0400

    feat: added Connection config parsing

--- pkg/connection/connection.go
+++ pkg/connection/connection.go
@@ -1,9 +1,46 @@
 package connection
 
-var Connectioners = map[string]Connectioner{}
+import (
+	"fmt"
+
+	log "github.com/sirupsen/logrus"
+	"gopkg.in/yaml.v3"
+)
+
+var Registry = map[string]func(string) Connectioner{}
+var Connections = map[string]Connectioner{}
+var Errors = []error{}
+
+func ParseConfig(raw map[string]map[string]interface{}) {
+	count := 0
+	for name, pluginConf := range raw {
+		for pluginName, pluginMap := range pluginConf {
+			f, ok := Registry[pluginName]
+			if !ok {
+				continue
+			}
+
+			p := f(name)
+			pluginYaml, err := yaml.Marshal(pluginMap)
+			if err != nil {
+				panic(err)
+			}
+
+			err = yaml.Unmarshal(pluginYaml, p)
+			if err != nil {
+				panic(err)
+			}
+
+			log.WithField("connection", fmt.Sprintf("%#v", p)).Debug("parsed connection")
+			Connections[name] = p
+			count++
+		}
+	}
+	log.Infof("parsed %d connections", count)
+}
 
 func GetInstance(name string) Connectioner {
-	if c, ok := Connectioners[name]; !ok {
+	if c, ok := Connections[name]; !ok {
 		return nil
 	} else {
 		return c
