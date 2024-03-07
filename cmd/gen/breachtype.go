8506ae6ebb9ce469d1aab6a502210d548c9e31fe
Yusuf A. Hasan Miyan <yusufh@protonmail.com>
2024-03-07 23:27:16 +0400

    fix the breachtype gen

--- cmd/gen/breachtype.go
+++ cmd/gen/breachtype.go
@@ -13,11 +13,11 @@ func BreachType(breachTypes []string) {
 	log.Println("Generating breach type funcs -", strings.Join(breachTypes, ","))
 
 	breachTypeFile := "breach_gen.go"
-	breachTypeFullFilePath := filepath.Join(getScriptPath(), "..", "..", "pkg", "result", breachTypeFile)
+	breachTypeFullFilePath := filepath.Join(getScriptPath(), "..", "..", "pkg", "breach", breachTypeFile)
 	if err := os.Remove(breachTypeFullFilePath); err != nil && !os.IsNotExist(err) {
 		log.Fatalln(err)
 	}
-	createFileWithString(breachTypeFullFilePath, "package result\n")
+	createFileWithString(breachTypeFullFilePath, "package breach\n")
 
 	for _, bt := range breachTypes {
 		appendFileContent(breachTypeFullFilePath, breachTypeFuncs(bt))
